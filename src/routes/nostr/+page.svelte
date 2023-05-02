adef9c227cd75e35487605bd245d547a6578d948
Jack Chakany <jack@chaker.net>
2023-05-02 08:34:57 -0400

    Add nostr route

---
+++ src/routes/nostr/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+    import { browser } from "$app/environment"
+
+	export const prerender = true;
+
+	if (browser)
+        goto("nostr:npub1chakany8dcz93clv4xgcudcvhnfhdyqutprq2yh72daydevv8zasmuhf02");
+</script>
+
+Redirecting...
